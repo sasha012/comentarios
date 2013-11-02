6a51985ef9249f1443b0cf9a3fc142d171a24ea5
sasha012 <sashaarman1994@gmail.com>
2013-11-02 20:00:44 +0100

    segon commit

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+$connection=mysqli_connect('localhost', 'root', 'root', 'COMENTARIOS')or die("mysql muere");
+$resultado=$connection->query("SELECT texto FROM comentario");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,27 +9,15 @@
     <link rel="stylesheet" type="text/css" href="estilos.css">
     <meta charset="UTF-8">
     <script src="jquery.js"></script>
-<script src="script.js"></script>
-
+    <script src="script.js"></script>
 </head>
-
 <body>
 <h1>Comentarios</h1>
 <textarea placeholder="aqui se escribe" id="comentarios"></textarea>
-
 <button id="enviar">enviar</button>
-
+<?php
+$arrayResultado=mysqli_fetch_array($resultado);
+?>
 <div id="comentarios-escritos"></div>
-
-
-
-
-
-
-
-
-
-
-
 </body>
 </html>
